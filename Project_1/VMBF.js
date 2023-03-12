084cc82b42b1785509836984324b9a48c28aa501
ZheniaM <108816223+ZheniaM@users.noreply.github.com>
2023-03-12 21:22:49 +0500

    Project_1
    Added project 1

---
+++ Project_1/VMBF.js
@@ -0,0 +1,157 @@
+function get_argv(){
+    if (process.argv[2] !== '--mod'){
+        return {
+            name: process.argv[2],
+            mod: 8,
+            input: `${process.argv[3]},${process.argv[4]},`,
+        };
+    }
+    switch (process.argv[3]){
+        case '8':
+        case '16':
+        case '32':
+            return {
+                name: process.argv[4],
+                mod: Number(process.argv[3]),
+                input: `${process.argv[5]},${process.argv[6]},`,
+            }
+        default:
+            console.log('[-] Invalid argument');
+            console.log('[-] Argument of --mod could be 8 or 16 or 32');
+            console.log('Default argument is 8')
+            process.exit(1);
+    }
+}
+function get_data_from_file(filename){
+    const fs = require('fs');
+    try {
+        const DATA = fs.readFileSync(filename, 'utf8');
+        return DATA;
+    } catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
+}
+function add_data_to_memory(memory, data, END){
+    let j = 0;
+    for (let i = 0; i < data.length; i++){
+        switch (data[i]){
+            case "+":
+            case "-":
+            case ">":
+            case "<":
+            case ".":
+            case ",":
+                memory[j++] = data[i];
+                break;
+            case "[":
+                memory[j++] = data[i];
+                memory[END]++;
+                break;
+            case "]":
+                if (memory[END] === 0){
+                    console.log("[-] There is an unopened bracket");
+                    console.exit(1);
+                }
+                memory[END]--;
+                memory[j++] = data[i];
+            default:
+                break;
+        }
+    }
+    if (memory[END]){
+        console.log('[-] Not enough brackets');
+        process.exit(1);
+    }
+    memory[END] = 0;
+}
+function do_bfcode(memory, START, END, MAXVALUE, input){
+    let cip = 0;
+    let mip = START;
+    let output = "";
+    let sip = END;
+    while (cip < START){
+        switch (memory[cip]){
+            case '+':
+                cip++;
+                if (memory[mip] === MAXVALUE - 1){
+                    memory[mip] = 0;
+                    break;
+                }
+                memory[mip]++;
+                break;
+            case '-':
+                cip++;
+                if (memory[mip] === 0){
+                    memory[mip] = MAXVALUE - 1;
+                    break;
+                }
+                memory[mip]--;
+                break;
+            case '>':
+                cip++;
+                if (mip === END - 1){
+                    mip = START;
+                    break;
+                }
+                mip++;
+                break;
+            case '<':
+                cip++
+                if (mip === START){
+                    mip = END - 1;
+                    break;
+                }
+                mip--;
+                break;
+            case '.':
+                cip++;
+                output += String.fromCharCode(memory[mip]);
+                break;
+            case ',':
+                cip++;
+                if (input.length === 0){
+                    console.log("[-] Not enough data in input");
+                    process.exit(1);
+                }
+                memory[mip] = input[0].charCodeAt();
+                input = input.slice(1, input.length);
+                break;
+            case '[':
+                cip++;
+                if (!memory[mip]){
+                    memory[sip]++;
+                    while (memory[sip] !== 0){
+                        if (memory[cip] === '['){
+                            memory[sip]++;
+                        } else if (memory[cip] === ']'){
+                            memory[sip]--;
+                        }
+                        cip++;
+                    }
+                    break;
+                }
+                memory[sip++] = cip - 1;
+                break;
+            case ']':
+                cip = memory[--sip];
+                memory[sip] = 0;
+                break;
+        }
+    }
+    console.log(output);
+}
+
+(function main(){
+    const MEMSIZE = 30_000;
+    const argue = get_argv();
+    const filename = argue.name;
+    const mod = argue.mod;
+    let input = argue.input;
+    let datafile = get_data_from_file(filename);
+    const MINIP = datafile.match(/[+<>,.[\]\-]/g).length;
+    const MAXIP = MINIP + MEMSIZE;
+    let memory = new Array(MAXIP + MINIP).fill(0);
+    add_data_to_memory(memory, datafile, MINIP);
+    do_bfcode(memory, MINIP, MAXIP, 2 ** mod, input);
+})();
